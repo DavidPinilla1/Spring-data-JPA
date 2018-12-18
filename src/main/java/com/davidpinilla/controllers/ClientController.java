8f34e9775c04e868283e520766832b0dcaaff5aa
David Pinilla <metroid_300@hotmail.com>
2018-12-18 20:00:27 +0100

    clients list view added

--- src/main/java/com/davidpinilla/controllers/ClientController.java
+++ src/main/java/com/davidpinilla/controllers/ClientController.java
@@ -1,6 +1,7 @@
 package com.davidpinilla.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class ClientController {
 	@RequestMapping(value = "list", method = RequestMethod.GET)
 	public String list(Model theModel) {
 		theModel.addAttribute("title", "Client list");
+		theModel.addAttribute("clients", clientDAO.findAll());
 		return "list";
 	}
 }
--- src/main/java/com/davidpinilla/models/dao/ClientDAO.java
+++ src/main/java/com/davidpinilla/models/dao/ClientDAO.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import ch.qos.logback.core.net.server.Client;
 
-@Repository("ClientDAOJPA")
+@Repository
 public class ClientDAO implements ClientDAOInterface{
 	@PersistenceContext
 	private EntityManager em;
