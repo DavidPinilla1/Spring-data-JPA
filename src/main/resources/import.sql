f49faea244dc7cc292e7526baee0c711279c111e
David Pinilla <metroid_300@hotmail.com>
2019-01-01 21:26:40 +0100

    added layout with thymeleaf fragments to the view

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO clients (id, name, lastname, email, create_at) VALUES(1, 'David', 'Pinilla', 'contact@davidpinilla.com', '2018-12-18');
-INSERT INTO clients (id, name, lastname, email, create_at) VALUES(2, 'John', 'Doe', 'example@mail.com', '2018-12-18');
