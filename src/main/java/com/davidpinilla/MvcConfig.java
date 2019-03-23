593b42fcfa4e15048739eea8c5cf9f9010b6c0f2
DavidPinilla1 <metroid_300@hotmail.com>
2019-03-23 22:28:04 +0100

    testing commit working properly

--- src/main/java/com/davidpinilla/MvcConfig.java
+++ src/main/java/com/davidpinilla/MvcConfig.java
@@ -1,5 +1,5 @@
 package com.davidpinilla;
-
+ 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
